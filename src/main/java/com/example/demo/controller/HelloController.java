195a371b50c254b52715f893d09fe715c5d765b0
zhy <zhi_da_di_yu@163.com>
2019-02-26 22:10:42 +0800

    list

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,10 +3,22 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(){
-        return "你好";
+
+    //public String hello(){
+    //    return "你好";
+    //}
+    public Map<String,String> hello(){
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("code","1");
+        map.put("msg","hello world");
+        return map;
     }
+
 }
